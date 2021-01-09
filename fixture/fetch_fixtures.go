0eb59a4cf2ab0b1a12d15c10f304d75e7e1e8b38
Ogün Babacan <ogun.babacan@trendyol.com>
2021-01-10 02:05:22 +0300

    wip

--- fixture/fetch_fixtures.go
+++ fixture/fetch_fixtures.go
@@ -5,7 +5,10 @@ import (
 	"log"
 )
 
-func FetchFixtures(ctx context.Context) error {
+// PubSubMessage is the payload of a Pub/Sub event.
+type PubSubMessage struct {}
+
+func FetchFixtures(ctx context.Context, m PubSubMessage) error {
 	log.Printf("Hello")
 	return nil
 }
