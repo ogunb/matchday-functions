1b6ae8c76589cf730ae40cbd4a4c60125523300b
Ogün Babacan <ogun.babacan@trendyol.com>
2021-01-10 14:49:56 +0300

    wip

--- go.mod
+++
@@ -1,9 +0,0 @@
-module matchday
-
-go 1.13
-
-replace github.com/ogunb/matchday-functions/reminder => ./reminder
-
-replace github.com/ogunb/matchday-functions/fixture => ./fixture
-
-require github.com/ogunb/matchday-functions/fixture v0.0.0-00010101000000-000000000000 // indirect
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// import reminder "github.com/ogunb/matchday-functions/reminder"
-import fixture "github.com/ogunb/matchday-functions/fixture"
-
-func main() {
-	// reminder.SmsUser(nil, reminder.PubSubMessage{ Message: []byte("ABC") });
-	fixture.FetchFixtures(nil);
-}
