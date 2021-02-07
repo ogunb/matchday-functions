5602f87de739b574b0f580edd2e06108d040e15d
Ogün Babacan <ogun.babacan@trendyol.com>
2021-02-07 22:21:53 +0300

    feat: add base for db helpers.

--- telegram-message-handler/db/firestore.py
+++ telegram-message-handler/db/firestore.py
@@ -1,4 +1,28 @@
 from google.cloud import firestore
 
-client = firestore.Client()
-print(client)
+db = firestore.Client()
+
+def create_team_document():
+  print("create_team")
+
+def update_team_metadata():
+  print("team")
+
+def add_follower(team_id, chat_id):
+  has_error = False
+  message = "Successfully added."
+
+  team_document_ref = db.collection("teams").document(str(team_id))
+  doc = team_document_ref.get(["followers"])
+  if doc.exists:
+    print(f"Document data: {doc.to_dict()}")
+  else:
+    print(u"No such document!")
+
+  return { has_error, message }
+
+def remove_follower():
+  print("remove_follower")
+
+
+print(add_follower(123, 123))
