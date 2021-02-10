64c587d9c6623431ac75e89e628d4152742acd6e
Ogün Babacan <ogun.babacan@trendyol.com>
2021-02-10 22:03:22 +0300

    feat: add credentials shell script

--- fixture/cmd/main.go
+++ fixture/cmd/main.go
@@ -1,21 +1,8 @@
 package main
 
-import (
-	"log"
-
-	"github.com/joho/godotenv"
-	// "github.com/ogunb/matchday-functions/fixture/queue"
-)
-
 import "github.com/ogunb/matchday-functions/fixture"
 
 func main() {
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-
 	// queue.CreateTask()
 	fixture.FetchFixtures(nil, fixture.PubSubMessage{})
 }
