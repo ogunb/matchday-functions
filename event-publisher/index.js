fb613bd1df18fa90d6b0bd98ac42367b09f5e996
Ogün Babacan <ogun.babacan@trendyol.com>
2021-02-21 13:25:15 +0300

    feat: event publisher

--- event-publisher/index.js
+++ event-publisher/index.js
@@ -1,12 +1,20 @@
+const { PubSub } = require('@google-cloud/pubsub');
+const pubSubClient = new PubSub();
+
 function publishEvent(req, res) {
-  const {
-    topicName,
-    ...rest
-  } = req.body;
+  const { topicName, ...rest } = req.body;
+  const data = JSON.stringify(rest);
+  const dataBuffer = Buffer.from(data);
 
-  console.log({ topicName, rest })
-
-  res.status(200).send("lol");
+  console.log(`Publishing to ${topicName} with ${data}...`);
+  pubSubClient.topic(topicName).publish(dataBuffer).then(messageId => {
+    console.log(`Message ${messageId} published.`);
+    res.sendStatus(200)
+  }).catch(error => {
+    console.error(`Received error while publishing: ${error.message}`);
+    process.exitCode = 1;
+    res.sendStatus(500)
+  })
 }
 
 exports.publishEvent = publishEvent;
