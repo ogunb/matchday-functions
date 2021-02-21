3c5b10c1bbb186e403f2bc5c40dcba9adddd496d
Ogün Babacan <ogun.babacan@trendyol.com>
2021-02-21 18:02:57 +0300

    log

--- follower-change-handler/index.js
+++ follower-change-handler/index.js
@@ -7,7 +7,10 @@ exports.handleFollowerChange = (event, context) => {
 
   const hadZeroFollowers = !oldFollowers || oldFollowers.length === 0
   const hasMoreThanOneFollowers = !!currentFollowers && currentFollowers.length >= 1
-  console.log(event.value.fields.metadata)
+  console.log(event.value.fields.metadata.mapValue.fields.id)
+  console.log(event.value.fields.metadata.mapValue.fields.id.value)
+  console.log(event.value.fields.metadata.mapValue.fields.name)
+  console.log(event.value.fields.metadata.mapValue.fields.name.value)
   if (hadZeroFollowers && hasMoreThanOneFollowers) {
     // fetch(`${process.env.PROJECT_URL}/team-fixture-handler`, {
     //   method: 'POST',
